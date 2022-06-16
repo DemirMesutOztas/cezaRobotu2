edc774cf2f57edd01426d511c08063c3081e5a2b
Gogle Öztaş <demiroztas06@hotmail.com>
2022-06-16 10:21:11 +0300

    hesaplama

--- cezaRobotu2/Form1.Designer.cs
+++ cezaRobotu2/Form1.Designer.cs
@@ -42,26 +42,16 @@ namespace cezaRobotu2
             this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.comboBox5 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.label7 = new System.Windows.Forms.Label();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.label8 = new System.Windows.Forms.Label();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.label9 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.cezaLimitleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -113,6 +103,7 @@ namespace cezaRobotu2
             this.button1.TabIndex = 8;
             this.button1.Text = "Hesapla";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -223,6 +214,7 @@ namespace cezaRobotu2
             this.comboBox3.Size = new System.Drawing.Size(456, 24);
             this.comboBox3.TabIndex = 14;
             this.comboBox3.Text = "Seçiniz";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
             // 
             // comboBox4
             // 
@@ -237,6 +229,7 @@ namespace cezaRobotu2
             this.comboBox4.Size = new System.Drawing.Size(459, 24);
             this.comboBox4.TabIndex = 15;
             this.comboBox4.Text = "Seçiniz";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
             // 
             // comboBox5
             // 
@@ -261,30 +254,6 @@ namespace cezaRobotu2
             this.label6.TabIndex = 17;
             this.label6.Text = "İki yıl içerisinde tekrarlandı mı?";
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton1.Location = new System.Drawing.Point(842, 48);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(53, 20);
-            this.radioButton1.TabIndex = 18;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Evet";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton2.Location = new System.Drawing.Point(936, 48);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(59, 20);
-            this.radioButton2.TabIndex = 19;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Hayır";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -295,30 +264,6 @@ namespace cezaRobotu2
             this.label7.TabIndex = 20;
             this.label7.Text = "Ölüm var mı?";
             // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton3.Location = new System.Drawing.Point(842, 105);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(53, 20);
-            this.radioButton3.TabIndex = 21;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Evet";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton4.Location = new System.Drawing.Point(936, 105);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(59, 20);
-            this.radioButton4.TabIndex = 22;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Hayır";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
@@ -328,30 +273,6 @@ namespace cezaRobotu2
             this.label8.Size = new System.Drawing.Size(200, 16);
             this.label8.TabIndex = 23;
             this.label8.Text = "Ürün güvenliğini etkiliyor mu?";
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton5.Location = new System.Drawing.Point(842, 161);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(53, 20);
-            this.radioButton5.TabIndex = 24;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Evet";
-            this.radioButton5.UseVisualStyleBackColor = true;
-            // 
-            // radioButton6
-            // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton6.Location = new System.Drawing.Point(936, 158);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(59, 20);
-            this.radioButton6.TabIndex = 25;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "Hayır";
-            this.radioButton6.UseVisualStyleBackColor = true;
             // 
             // label9
             // 
@@ -390,61 +311,52 @@ namespace cezaRobotu2
             this.cezaLimitleriToolStripMenuItem.Text = "Ceza Limitleri";
             this.cezaLimitleriToolStripMenuItem.Click += new System.EventHandler(this.cezaLimitleriToolStripMenuItem_Click);
             // 
-            // dataGridView1
+            // comboBox6
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5});
-            this.dataGridView1.Location = new System.Drawing.Point(677, 246);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 30;
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.comboBox6.Location = new System.Drawing.Point(820, 47);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 30;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
             // 
-            // Column1
+            // comboBox7
             // 
-            this.Column1.HeaderText = "Column1";
-            this.Column1.Name = "Column1";
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.comboBox7.Location = new System.Drawing.Point(820, 96);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(121, 21);
+            this.comboBox7.TabIndex = 31;
             // 
-            // Column2
+            // comboBox8
             // 
-            this.Column2.HeaderText = "Column2";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Column3";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Column4";
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Column5";
-            this.Column5.Name = "Column5";
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.comboBox8.Location = new System.Drawing.Point(820, 155);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(121, 21);
+            this.comboBox8.TabIndex = 32;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1176, 569);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox8);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.comboBox6);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.radioButton6);
-            this.Controls.Add(this.radioButton5);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.comboBox5);
             this.Controls.Add(this.comboBox4);
@@ -465,7 +377,6 @@ namespace cezaRobotu2
             this.Text = "Ceza Robotu";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -481,29 +392,20 @@ namespace cezaRobotu2
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton6;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem cezaLimitleriToolStripMenuItem;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        public System.Windows.Forms.ComboBox comboBox2;
+        public System.Windows.Forms.ComboBox comboBox3;
+        public System.Windows.Forms.ComboBox comboBox4;
+        public System.Windows.Forms.ComboBox comboBox5;
+        public System.Windows.Forms.ComboBox comboBox6;
+        public System.Windows.Forms.ComboBox comboBox7;
+        public System.Windows.Forms.ComboBox comboBox8;
+        public System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- cezaRobotu2/Form1.cs
+++ cezaRobotu2/Form1.cs
@@ -10,9 +10,18 @@ using System.Windows.Forms;
 
 namespace cezaRobotu2
 {
+   
     public partial class Form1 : Form
     {
-        public int altlimit;
+        public double altlimit=0;
+        public double sinif = 0;
+        public double hse = 0;
+        public double tekrar = 1;
+        public double olum = 1;
+        public double sonuc = 0;
+        public double ustlimit;
+
+
         Form2 secondForm = new Form2();
 
 
@@ -34,60 +43,177 @@ namespace cezaRobotu2
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            
+            
+               
+
+            
 
 
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox3.SelectedIndex)
+            {
+                case 0:
+                    sinif = 1;
+
+                    break;
+                case 1:
+                    sinif = 1.5;
+
+                    break;
+                case 2:
+                    sinif = 1.5;
+
+                    break;
+                case 3:
+                    sinif = 2;
+
+                    break;
+                case 4:
+                    sinif = 2.5;
+
+                    break;
+                case 5:
+                    sinif = 3;
+
+                    break;
+                case 6:
+                    sinif = 3;
+
+                    break;
+                case 7:
+                    sinif = 1;
+
+                    break;
+                case 8:
+                    sinif = 2;
+
+                    break;
+                case 9:
+                    sinif = 2.5;
+
+                    break;
+                case 10:
+                    sinif = 3;
+
+                    break;
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox4.SelectedIndex)
+            {
+                case 0:
+                    hse = 1;
+                    break;
+                case 1:
+                    hse = 2;
+                    break;
+                case 2:
+                    hse = 4;
+                    break;
+            }
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox6.SelectedItem.ToString() == "Evet")
+            {
+                tekrar = 2;
+            }
+            else
+            {
+                tekrar = 1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             switch (comboBox1.SelectedIndex)
             {
                 case 0:
-
-                    altlimit = (int)secondForm.dataGridView1.Rows[0].Cells[1].Value;
-                   
+                    if (comboBox2.SelectedItem.ToString() == "2021")
+                    {
+                        altlimit = Convert.ToInt32(secondForm.dataGridView1.Rows[0].Cells[1].Value);
+                        ustlimit = Convert.ToInt32(secondForm.dataGridView1.Rows[0].Cells[2].Value);
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "2022")
+                    {
+                        altlimit = Convert.ToInt32(secondForm.dataGridView1.Rows[0].Cells[3].Value);
+                        ustlimit = Convert.ToInt32(secondForm.dataGridView1.Rows[0].Cells[4].Value);
+                    }
+                    MessageBox.Show(altlimit.ToString());
                     break;
 
                 case 1:
-
-                    altlimit = (int)secondForm.dataGridView1.Rows[1].Cells[1].Value;
-
+                    if (comboBox2.SelectedItem.ToString() == "2021")
+                    {
+                        altlimit = (int)secondForm.dataGridView1.Rows[1].Cells[1].Value;
+                        ustlimit = Convert.ToInt32(secondForm.dataGridView1.Rows[1].Cells[2].Value);
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "2022")
+                    {
+                        altlimit = (int)secondForm.dataGridView1.Rows[1].Cells[3].Value;
+                        ustlimit = Convert.ToInt32(secondForm.dataGridView1.Rows[1].Cells[4].Value);
+                    }
+                    MessageBox.Show(altlimit.ToString());
                     break;
 
                 case 2:
 
                     altlimit = (int)secondForm.dataGridView1.Rows[2].Cells[1].Value;
-
+                    MessageBox.Show(altlimit.ToString());
                     break;
 
                 case 3:
 
                     altlimit = (int)secondForm.dataGridView1.Rows[3].Cells[1].Value;
-
+                    MessageBox.Show(altlimit.ToString());
                     break;
 
                 case 4:
 
                     altlimit = (int)secondForm.dataGridView1.Rows[4].Cells[1].Value;
-
+                    MessageBox.Show(altlimit.ToString());
                     break;
 
                 case 5:
 
                     altlimit = (int)secondForm.dataGridView1.Rows[5].Cells[1].Value;
-
+                    MessageBox.Show(altlimit.ToString());
                     break;
 
                 case 6:
 
                     altlimit = (int)secondForm.dataGridView1.Rows[6].Cells[1].Value;
-
+                    MessageBox.Show(altlimit.ToString());
                     break;
 
                 case 7:
 
                     altlimit = (int)secondForm.dataGridView1.Rows[7].Cells[1].Value;
-
+                    MessageBox.Show(altlimit.ToString());
                     break;
             }
+
             
-           
+            if (sonuc > ustlimit || (comboBox7.SelectedItem.ToString() == "Evet"))
+            {
+                sonuc = ustlimit;
+            }
+            else
+            {
+                sonuc = altlimit * sinif * hse * tekrar;
+            }
+
+            textBox1.Text = sonuc.ToString();
+
+
         }
     }
 }
